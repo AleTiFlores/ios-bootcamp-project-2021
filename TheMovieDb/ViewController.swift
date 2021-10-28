00f6bf4f5754c73b50dc83a26018ca8013035943
Iktan <trejographics@gmail.com>
2021-10-28 16:30:27 -0500

    Testing TMDB api requests.
    - I've made the setup and added the logic to make get requests.

--- TheMovieDb/ViewController.swift
+++ TheMovieDb/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        guard let url = URL(string: "https://api.themoviedb.org/3/discover/movie?api_key=3f2d000acd208182b31eb1e5c2903ab8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate") else { return }
+        let request = URLRequest(url: url)
+        performRequest(request: request)
     }
 
-
+    private func performRequest(request: URLRequest) {
+        let session = URLSession.shared
+        session.dataTask(with: request) { (sessionData, sessionResponse, error) in
+            guard let response = sessionResponse,
+                  let data = sessionData
+            else { return }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                print(json)
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+    
 }
 
